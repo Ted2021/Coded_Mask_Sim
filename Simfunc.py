adc8ff7c7f13e086fadbc90915a44a2f980da16c
kiyomototakuto <kiyomototakuto@gmail.com>
2023-08-07 12:55:47 +0900

    Debug util.py

--- Simfunc.py
+++ Simfunc.py
@@ -6,22 +6,26 @@ def def_mask(mask_size):
     mask = np.random.randint(0,2,mask_size)
     return mask
 
-def  def_source_pos(*args):
-    return args
+def  def_source_pos(ang, power):
+    tnkyu = np.zeros(180)
+    for (theta, flux) in zip(ang, power):
+        tnkyu[theta] = flux
+    return tnkyu
 
-def create_detec_image(mask_size, mask, source_pos, fl):
+def create_detec_image(mask_size, mask, ang, power, fl):
     detec = np.zeros(mask_size*3)
     center = round(len(detec)/2)
 
     #fl = mask_size
-    for item in source_pos:
-        cell = round(fl*math.tan(math.radians(item)))
+    for (theta, flux) in zip(ang, power):
+        cell = round(fl*math.tan(math.radians(theta)))
+        print("cell num : {}".format(cell))
         for i in range(len(mask)):
-            if center + item + i < len(detec):
-                detec[center + item + i - int(len(mask)/2)] = mask[i]
+            if center + cell + i < len(detec):
+                detec[center + cell + i - int(len(mask)/2)] += flux * mask[i]
     return detec
 
-def decode_image(mask, detec):
+def decode_image(mask, detec, fl):
     decode_image = []
     for i in range(len(detec)):
         temp = 0
@@ -32,4 +36,11 @@ def decode_image(mask, detec):
             for j in range(len(detec) - i):
                 temp += detec[i+j] * mask[j]
         decode_image.append(temp)
-    return decode_image
+    
+    pos = [x for x in range(len(decode_image))]
+    center = round(len(detec)/2)
+    source = []
+    for i in range(len(pos)):
+        source.append(math.degrees(math.atan((pos[i]-center+ int(len(mask)/2))/fl)))
+
+    return source, decode_image
